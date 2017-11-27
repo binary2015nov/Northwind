80113451a058f62b956dfdb47368fc7285afc7fb
Demis Bellot <demis.bellot@gmail.com>
2017-11-27 18:37:12 -0500

    remove unnecessary ServiceStack.Configuration namespace + merge includes

--- src/Northwind/Startup.cs
+++ src/Northwind/Startup.cs
@@ -1,20 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 using Funq;
 using ServiceStack;
 using ServiceStack.Data;
 using ServiceStack.Admin;
 using ServiceStack.OrmLite;
-using ServiceStack.Configuration;
 using Northwind.ServiceInterface;
 
 namespace Northwind
